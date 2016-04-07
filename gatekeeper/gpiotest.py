923001dfd3795d97161477ae4a8c149a8aca100b
Eric Whyne <ericwhyne@gmail.com>
2016-04-06 22:21:31 -0400

    test

--- gatekeeper/gpiotest.py
+++ gatekeeper/gpiotest.py
@@ -3,6 +3,6 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(23, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 while True:
    GPIO.wait_for_edge(23, GPIO.RISING, bouncetime=500)
-   print("Cage Door is Open")
-   GPIO.wait_for_edge(23, GPIO.FALLING, bouncetime=500)
    print("Cage Door is Closed")
+   GPIO.wait_for_edge(23, GPIO.FALLING, bouncetime=500)
+   print("Cage Door is Open")
