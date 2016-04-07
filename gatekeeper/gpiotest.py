7de76c0ed0e20b3aa3820b2f866969a0d4d93264
Eric Whyne <ericwhyne@gmail.com>
2016-04-06 22:18:01 -0400

    test

--- gatekeeper/gpiotest.py
+++ gatekeeper/gpiotest.py
@@ -1,15 +1,8 @@
 import RPi.GPIO as GPIO
-
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(23, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
-
-def doorOpen(channel):
-    print("Door Opened!")
-    GPIO.wait_for_edge(23, GPIO.FALLING)
-    print("Door Closed!")
-
-print("Note how the bouncetime affects the button press")
-GPIO.add_event_detect(23, GPIO.RISING, callback=doorOpen, bouncetime=1000)
-
 while True:
-    pass
+   GPIO.wait_for_edge(23, GPIO.RISING)
+   print("Button 1 Pressed")
+   GPIO.wait_for_edge(23, GPIO.FALLING)
+   print("Button 1 Released")
