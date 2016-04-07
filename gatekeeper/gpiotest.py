a9f25b075dd12ce723e65b61480db388d3088c6a
Eric Whyne <ericwhyne@gmail.com>
2016-04-06 21:59:34 -0400

    test

--- gatekeeper/gpiotest.py
+++ gatekeeper/gpiotest.py
@@ -12,7 +12,7 @@ GPIO.add_event_detect(23, GPIO.RISING, callback=printFunction, bouncetime=300)
 
 while True:
     GPIO.wait_for_edge(24, GPIO.FALLING)
-    print(“Button 2 Pressed”)
+    print("Button 2 Pressed")
     GPIO.wait_for_edge(24, GPIO.RISING)
-    print(“Button 2 Released”)
+    print("Button 2 Released")
     GPIO.cleanup()
