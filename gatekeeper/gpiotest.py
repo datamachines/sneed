4d5d40c9d16b0eaf581a58e3e8d3279c5014c816
Eric Whyne <ericwhyne@gmail.com>
2016-04-06 22:19:20 -0400

    test

--- gatekeeper/gpiotest.py
+++ gatekeeper/gpiotest.py
@@ -3,6 +3,6 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(23, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 while True:
    GPIO.wait_for_edge(23, GPIO.RISING)
-   print("Button 1 Pressed")
+   print("Cage Door is Open")
    GPIO.wait_for_edge(23, GPIO.FALLING)
-   print("Button 1 Released")
+   print("Cage Door is Closed")
