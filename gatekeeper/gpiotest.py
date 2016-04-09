c740283f0d82b3cfe005a78e8506ef7fd1a0744f
Eric Whyne <ericwhyne@gmail.com>
2016-04-09 00:33:47 -0400

    derp

--- gatekeeper/gpiotest.py
+++ gatekeeper/gpiotest.py
@@ -4,5 +4,7 @@ GPIO.setup(23, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 while True:
    GPIO.wait_for_edge(23, GPIO.RISING, bouncetime=500)
    print("Cage Door is Closed")
+   print GPIO.input(23)
    GPIO.wait_for_edge(23, GPIO.FALLING, bouncetime=500)
    print("Cage Door is Open")
+   print GPIO.input(23)
