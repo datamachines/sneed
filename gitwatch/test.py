d86700650d9f061b29a1186c495f1e04c8e9e6b1
Eric Whyne <ericwhyne@gmail.com>
2016-04-10 17:29:21 -0400

    worth keeping

--- gitwatch/test.py
+++ gitwatch/test.py
@@ -1,43 +1,45 @@
 #!/usr/bin/python
+from __future__ import print_function
 from git import Repo
-repodir = "../vagrant-xdata/"
-
-repo = Repo(repodir)
-
-
-commitIDs = []
-#load in batches to prevent hangs? not sure if you should care - personally I wouldn't bother with this
-start, max_count = 0, 100
-while True:
-    text = repo.git.rev_list("master", max_count=max_count, skip=start)
-    lines = text.splitlines()
-    start += len(lines)
-    #append shas to commitIds
-    commitIDs += lines
-
-    if len(lines) < max_count:
-        break
-#print commitIDs[0]
-#print repo.git.show(commitIDs[0])
-
-
-'''
-import git
 from datetime import datetime
-repodir = "../vagrant-xdata/"
-repo = git.Git(repodir)
-log = repo.log()
+import yaml
 
-repo = git.Repo(repodir)
+configfile = "config-example.yaml"
+runfile = "runfile-example.yaml"
 
-log = repo.heads.origin.log()
-for entry in log:
-    print "time:", datetime.utcfromtimestamp(entry[3][0])
+# Set up configuraiton
+conf = yaml.safe_load(open(configfile))
+repo = Repo(conf['repodir'])
+now = datetime.now()
 
-heads = repo.heads
-print heads
-master = heads.master
-log = master.log()
-print log[0][0]
-print log[-1]
-'''
+def write_runfile(run):
+    try:
+        with open(runfile, 'w') as outfile:
+            outfile.write( yaml.dump(run, default_flow_style=False) )
+    except IOError:
+        logtime = datetime.now().isoformat()
+        print(logtime, "ERROR - Unable to write runfile.")
+        exit(1)
+
+try:
+    run = yaml.safe_load(open(runfile))
+except IOError:
+    run = dict(lastrun = int(now.strftime("%s")))
+    logtime = datetime.now().isoformat()
+    print(logtime, "First run, just creating runfile and exiting.")
+    print(logtime,
+        "Tracking new commits from this moment in time:",
+        now.isoformat())
+    write_runfile(run)
+    exit(0)
+
+commits = list(repo.iter_commits('master'))
+
+for commit in commits:
+    if commit.committed_date > run['lastrun']:
+        print(commit.author.email)
+        print(datetime.utcfromtimestamp(commit.committed_date).isoformat())
+
+run['lastrun'] = int(now.strftime("%s"))
+write_runfile(run)
+exit(0)
