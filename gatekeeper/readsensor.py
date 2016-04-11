e5a8e5e1fa9abf2ab6051877c6d0d4894acaa43c
Eric Whyne <ericwhyne@gmail.com>
2016-04-11 14:59:03 -0400

    cleanup

--- gatekeeper/readsensor.py
+++ gatekeeper/readsensor.py
@@ -1,6 +1,22 @@
 import RPi.GPIO as GPIO
+import sys
+import json
+
 GPIO.setmode(GPIO.BCM)
+
+sensor_map_file = sys.argv[1]
+
+try:
+  sensor_map = json.load(open(sensor_map_file))
+except Exception, e:
+  print "\nFAILED! JSON error in file %s" % sensor_map_file
+  print " Details: %s" % str(e)
+  sys.exit(1)
+
+print sensor_map
+
 GPIO.setup(23, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+
 while True:
    GPIO.wait_for_edge(23, GPIO.RISING, bouncetime=500)
    print("Cage Door is Closed")
---
+++ gatekeeper/v2-sneed-gatekeeper.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+import yaml
+import requests
+import json
+import RPi.GPIO as GPIO
+from datetime import datetime
+
+configfile = "../../sneedconfig.yaml"
+conf = yaml.safe_load(open(configfile))
+
+print conf
+payload = {
+"text":conf['message'],
+"channel":conf['channel'],
+"icon_emoji":conf['icon_emoji'],
+"username": conf['username']
+}
+
+def slack(message):
+    payload['text'] = message
+    r = requests.post(conf['webhookurl'], data = json.dumps(payload))
+    print r.text
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(23, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+while True:
+   GPIO.wait_for_edge(23, GPIO.RISING, bouncetime=500)
+   time = datetime.now().isoformat()
+   print("Cage Door is Closed " + time)
+   slack("Cage Door is Closed " + time)
+   GPIO.wait_for_edge(23, GPIO.FALLING, bouncetime=500)
+   time = datetime.now().isoformat()
+   print("Cage Door is Open " + time)
+   slack("Cage Door is Open " + time)
